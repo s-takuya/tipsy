9c512cd66dee34f0b91ef15ccc894d78364b931d
Jun Okada <jun.okada0203@gmail.com>
2016-10-15 23:00:18 +0900

    Add relation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   has_attached_file :image, s3_protocol: :https
   validates_attachment_presence :image
   validates_attachment_content_type :image, content_type: %r{\Aimage\/.*\Z}
+
+  has_many :tag_posts
+  has_many :tags, through: :tag_posts
 end
