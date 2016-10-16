8fdcadf4a4f2a236a8160f424fd6d63ed9940d89
s-takuya <takuyashiratake0622@icloud.com>
2016-10-16 12:54:44 +0900

    Set s3 setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,12 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_host_name: 's3-ap-northeast-1.amazonaws.com',
+    s3_credentials: { bucket: 'get-tipsy' }
+  }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
