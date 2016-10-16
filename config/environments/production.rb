e815dd9a6da29d24c6af97296a18a2272a231607
s-takuya <takuyashiratake0622@icloud.com>
2016-10-16 14:16:36 +0900

    Change production setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "tipsy_#{Rails.env}"
+  # config.active_job.queue_name_prefix = 'tipsy_#{Rails.env}'
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
@@ -75,7 +75,7 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
@@ -83,8 +83,14 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
-    s3_region: 's3-ap-northeast-1.amazonaws.com',
-    s3_credentials: { bucket: 'get-tipsy' }
+    s3_region: ENV['AWS_S3_REGION'],
+    s3_credentials: {
+      s3_host_name: ENV['AWS_S3_HOST_NAME'],
+      bucket: ENV['S3_BUCKET_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      }
+    }
   }
 
   # Do not dump schema after migrations.
