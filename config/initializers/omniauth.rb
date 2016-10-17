f4a7cc3d3dfc08f91f944a7eb6f3b19ffc99a97d
Jun Okada <jun.okada0203@gmail.com>
2016-10-17 22:55:43 +0900

    Fix user system

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+    callback_path: '/auth/facebook/callback'
 end
